7c7f09d074e1af441d73b4c0d86a0a2707056181
Balaji S V <86550781+Balaji-S-V@users.noreply.github.com>
2023-08-07 16:44:24 +0530

    first-commit

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+public class LinkedList
+    {
+        private Node head;
+        private Node tail;
+
+        class Node
+        {
+            Node next;
+            int value;
+            public Node(int value)
+            {
+                this.value = value;
+            }
+        }
+        public LinkedList(int value)
+        {
+            Node newNode = new Node(value);
+            head = newNode;
+            tail = newNode;
+        }
+
+        LinkedList list = new LinkedList(4);
+    }
